52a05343b37c9298e37c69d294d61bc9d55966a6
StpehenChinag <stephenchinag@gmail.com>
2024-04-17 13:03:24 +0100

    best pattern

--- app.js
+++
@@ -1,5 +0,0 @@
-function add(a, b) {
-    return a + b;
-}
-var result = add(10, 7);
-console.log(result);
--- app.ts
+++
@@ -1,5 +0,0 @@
-function add(a: number, b: number) {
-	return a + b;
-}
-const result = add(10, 7);
-console.log(result);
