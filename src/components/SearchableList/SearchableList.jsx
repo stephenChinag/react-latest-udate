0dffc75caa8bdf3cfd169bdf21aaaeae9999fded
StpehenChinag <stephenchinag@gmail.com>
2024-04-19 14:25:48 +0100

    search

--- src/components/SearchableList/SearchableList.jsx
+++ src/components/SearchableList/SearchableList.jsx
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function SearchableList({ items }) {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const searchResults = items.filter((item) =>
+    JSON.stringify(item).toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  //   function handleChange(e) {
+  //     setSearchTerm(e.target.value);
+  //   }
   return (
     <div className="searchable-list">
-      <input type="search" placeholder="Search" />
+      <input
+        type="search"
+        placeholder="Search"
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
       <ul>
-        {items.map((item) => (
-          <li key={item}>{item}</li>
+        {searchResults.map((item, index) => (
+          <li key={index}>{item.toString()}</li>
         ))}
       </ul>
     </div>
