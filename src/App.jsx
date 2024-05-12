48ed81270d3221286b28255d40a13928ef052427
StpehenChinag <stephenchinag@gmail.com>
2024-05-12 10:28:52 +0100

    p

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,10 @@
-import Accordion from "./components/Accordion/Accordion";
-import AccordionItem from "./components/Accordion/AccordionItem";
-import SearchableList from "./components/SearchableList/SearchableList";
-import { PLACES } from "./Place";
-import Place from "./Place";
+import React from "react";
+import Product from "./components/Product";
 
-function App() {
+export default function App() {
   return (
-    <main>
-      <section>
-        <h2> Why work with us</h2>
-        <Accordion className="accordion">
-          <Accordion.Item id="experience" className="accordion-item">
-            <Accordion.Title className="accordion-item-title">
-              We're working with local guides
-            </Accordion.Title>
-            <Accordion.Content className="accordion-item-content">
-              <article>
-                <p> You cant&apos;'t go wrong with us</p>
-                <p>
-                  We are in the business planning highly individualized vaction
-                </p>
-              </article>
-            </Accordion.Content>
-          </Accordion.Item>
-          <Accordion.Item id="local-guides" className="accordion-item">
-            <Accordion.Title className="accordion-item-title">
-              We're working with local guides
-            </Accordion.Title>
-            <Accordion.Content className="accordion-item-content">
-              <article>
-                <p> You cant&apos;'t go wrong with us</p>
-                <p>
-                  We are in the business planning highly individualized vaction
-                </p>
-              </article>
-            </Accordion.Content>
-          </Accordion.Item>
-        </Accordion>
-      </section>
-      <section>
-        <SearchableList items={PLACES} itemKeyFn={(item) => item.id}>
-          {(item) => <Place item={item} />}
-        </SearchableList>
-        <SearchableList items={["item 1", "Item 2"]} itemKeyFn={(item) => item}>
-          {(item) => item}
-        </SearchableList>
-      </section>
-    </main>
+    <div>
+      <Product />
+    </div>
   );
 }
-
-export default App;
---
+++ src/components/Product.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Product() {
+  return <div>Product Component</div>;
+}
+
+export default Product;
