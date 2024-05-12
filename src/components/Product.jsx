aa5b2e74f454f361cf00081a112445212f23ee1b
StpehenChinag <stephenchinag@gmail.com>
2024-05-12 12:51:41 +0100

    light

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
+import "./product.scss";
 function Product() {
-  return <div>Product Component</div>;
+  const [products, setProdducts] = useState([]);
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then((data) => data.json())
+      .then((result) => setProdducts(result));
+  }, []);
+
+  return (
+    <div>
+      <h1>Products Listed</h1>
+      <div className="card-container">
+        {products.map((p) => (
+          <div className="card">
+            <img src={p.image} alt="" className="card-image" />
+            <div className="card-content">
+              <h2 className="card-title"> {p.title}</h2>
+              <p className="card-price">{p.price}</p>
+            </div>
+
+            <button className="button">checkout</button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Product;
